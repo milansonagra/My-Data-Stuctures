d120a572a51a5d3c691d18f7a82de7110d8d5300
Milan Sonagra <32453665+milansonagra@users.noreply.github.com>
2018-03-23 18:54:06 +0530

    Add files via upload

---
+++ Greedy Algorithm/JOB_SCHEDULING.java
@@ -0,0 +1,177 @@
+
+import java.util.*;
+
+/**
+ *
+ * @author Milan
+ */
+class Entries {
+
+    private String names;
+    private int deadline;
+    private int profit;
+
+    public Entries(String name, int deadline, int profit) {
+        this.names = name;
+        this.deadline = deadline;
+        this.profit = profit;
+    }
+
+    public String getName() {
+        return names;
+    }
+
+    public int getDeadline() {
+        return deadline;
+    }
+
+    public int getProfit() {
+        return profit;
+    }
+}
+
+class Job_Scheduling {
+
+    private LinkedList<Entries> AllLists;
+    private String[] AllocatedTime;
+    private int NumberofRequests;
+    private int MaxTime;
+
+    public Job_Scheduling(LinkedList<Entries> L) {
+
+        AllLists = new LinkedList<Entries>();
+        int length_L = L.indexOf(L.getLast()) + 1;
+        Entries MostElagiableEntry;
+        Entries ObjZero = new Entries("", 0, 0);
+
+        for (int i = 0; i < length_L; i++) {
+            MostElagiableEntry = L.get(i);
+            int index_max = 0;
+            for (int j = 0; j < length_L; j++) {
+                if (MostElagiableEntry.getProfit() < L.get(j).getProfit()) {
+                    MostElagiableEntry = L.get(j);
+                    index_max = j;
+                }
+            }
+            L.set(index_max, ObjZero);
+            AllLists.addLast(MostElagiableEntry);
+        }
+        NumberofRequests = AllLists.indexOf(AllLists.getLast()) + 1;
+
+        int max_time = AllLists.getFirst().getDeadline();
+        for (int i = 1; i < length_L; i++) {
+            if (max_time < AllLists.get(i).getDeadline()) {
+                max_time = AllLists.get(i).getDeadline();
+            }
+        }
+        this.MaxTime = max_time;
+        AllocatedTime = new String[NumberofRequests];
+    }
+
+    public int maxProfit() {
+        int MaxProfit = 0;
+        AllocatedTime = new String[MaxTime];
+        Arrays.fill(AllocatedTime, null);
+        for (int i = 0; i < NumberofRequests; i++) {
+            Entries CurrentEntry = AllLists.get(i);
+            for (int j = 0; j < MaxTime - 1; j++) {
+                int index = (CurrentEntry.getDeadline() - 1) - j;
+                if (index >= 0 && null == AllocatedTime[index]) {
+                    AllocatedTime[index] = CurrentEntry.getName();
+                    MaxProfit += CurrentEntry.getProfit();
+                    break;
+                }
+            }
+        }
+        return MaxProfit;
+    }
+
+    public void showListOfAllocatedEntries() {
+        for (int i = 0; i < MaxTime; i++) {
+            if (AllocatedTime[i] == null) {
+                System.out.println("No Allocation in " + i + "-" + (i + 1));
+            } else {
+                System.out.println("Name of Candidate: " + AllocatedTime[i] + "\tTime Allocated: " + i + "-" + (i + 1));
+            }
+//    if(AllocatedTime[i] != null){
+//        System.out.println("Name of Candidate: " + AllocatedTime[i] + "\tTime Allocated: " + i + "-" + (i + 1));
+//    }
+        }
+    }
+}
+
+public class P5_1_JOB_SCHEDULING {
+
+    public static void main(String[] args) {
+
+        Entries Application1 = new Entries("a", 4, 20);
+        Entries Application2 = new Entries("b", 1, 10);
+        Entries Application3 = new Entries("c", 1, 40);
+        Entries Application4 = new Entries("d", 1, 30);
+        Entries Application5 = new Entries("e", 3, 15);
+
+        LinkedList<Entries> List = new LinkedList<Entries>();
+
+        List.add(Application1);
+        List.add(Application2);
+        List.add(Application3);
+        List.add(Application4);
+        List.add(Application5);
+
+        /*FOR ANALYSIS:*/ long startTime = System.currentTimeMillis();
+
+        Job_Scheduling HollA = new Job_Scheduling(List);
+
+        System.out.println("MAXIMUM PROFIT: " + HollA.maxProfit());
+        /*FOR ANALYSIS:*/ long endTime = System.currentTimeMillis();
+
+        System.out.println("\nTime Table of HallA:");
+        HollA.showListOfAllocatedEntries();
+
+        /*FOR ANALYSIS:*/ long executionTime = endTime - startTime;
+
+        /*FOR ANALYSIS:*/ System.out.println("Total Execution Time: " + executionTime + " ms");
+    }
+}
+/*
+
+
+        
+        int a = 10;
+//        Entries Application1 = new Entries("a", 4, 20);
+//        Entries Application2 = new Entries("b", 1, 10);
+//        Entries Application3 = new Entries("c", 1, 40);
+//        Entries Application4 = new Entries("d", 1, 30);
+//        Entries Application5 = new Entries("e", 3, 15);
+        Entries Application[] = new Entries[100];
+        for (int i = 0; i < 10*a; i++) {
+            Application[i] = new Entries("a" + i, (int) (Math.random() * a*10), (int) (Math.random() * 100));
+        }
+
+        LinkedList<Entries> List = new LinkedList<Entries>();
+
+//        List.add(Application1);
+//        List.add(Application2);
+//        List.add(Application3);
+//        List.add(Application4);
+//        List.add(Application5);
+//        List.add(Application6);
+        for (int i = 0; i < 10*a; i++) {
+            List.add(Application[i]);
+        }
+        /*FOR ANALYSIS:*\/ long startTime = System.currentTimeMillis();
+
+        Job_Scheduling HollA = new Job_Scheduling(List);
+
+        System.out.println("MAXIMUM PROFIT: " + HollA.maxProfit());
+        /*FOR ANALYSIS:*\/ long endTime = System.currentTimeMillis();
+
+        System.out.println("\nTime Table of HallA:");
+        HollA.showListOfAllocatedEntries();
+
+        /*FOR ANALYSIS:*\/ long executionTime = endTime - startTime;
+
+        /*FOR ANALYSIS:*\/ System.out.println("Total Execution Time: " + executionTime + " ms");
+  
+
+*/
---
+++ Greedy Algorithm/Prims.java
@@ -0,0 +1,130 @@
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Milan
+ */
+class Graph {
+
+    final private int numberOfNode;
+    private int[][] graph;
+    private int[] parent;
+    boolean[] visited;
+
+    Graph(int[][] matrix) {
+        graph = matrix;
+        numberOfNode = graph.length;
+        parent = new int[numberOfNode];
+        visited = new boolean[numberOfNode];
+    }
+
+    public int mstPrimes(int sorce) {
+        int Cost = 0, CurrentNode = sorce, ParentNode = Integer.MAX_VALUE;
+        ArrayList<Integer> ConsideredEdge = new ArrayList<>();
+        visited[sorce] = true;
+        for (int count = 0; count < numberOfNode; count++) {
+
+            for (int i = 0; i < numberOfNode; i++) {
+                int var = graph[CurrentNode][i];
+                ConsideredEdge.add(i + (count * numberOfNode), var);
+            }
+
+            int[] NodeWithMinimumCost = minWithIndex(ConsideredEdge);
+            Cost += NodeWithMinimumCost[0];
+            parent[CurrentNode] = ParentNode;
+            CurrentNode = NodeWithMinimumCost[1];
+            ParentNode = NodeWithMinimumCost[2];
+            visited[CurrentNode] = true;
+        }
+
+        return Cost;
+    }
+
+    public void printMST() {
+        System.out.print("MST: \nNode --> Reach From Node(Parent Node)\n");
+        for (int i = 0; i < numberOfNode; i++) {
+            System.out.printf("%d --> %d\n", i, parent[i]);
+        }
+    }
+
+    private int[] minWithIndex(ArrayList<Integer> AL) {
+        int[] Answer = new int[3];
+        Answer[0] = Integer.MAX_VALUE;
+        boolean flag = false;
+        for (int i = 0; i < AL.size(); i++) {
+            if (AL.get(i) != 0 && !visited[i % numberOfNode] && Answer[0] > AL.get(i)) {
+                Answer[0] = AL.get(i);
+                Answer[1] = i % numberOfNode;
+                Answer[2] = i / numberOfNode;
+                flag = true;
+            }
+        }
+
+        if (flag) {
+            return Answer;
+        } else {
+            Answer[0] = 0;
+            return Answer;
+        }
+    }
+}
+
+public class P5_2_Prims {
+
+    public static void main(String[] args) {
+
+        int graph[][] = new int[][]{
+            {0, 10, 9, 4, 2},
+            {10, 0, 20, 3, Integer.MAX_VALUE},
+            {9, 20, 0, Integer.MAX_VALUE, 5},
+            {4, 3, Integer.MAX_VALUE, 0, Integer.MAX_VALUE},
+            {2, Integer.MAX_VALUE, 5, Integer.MAX_VALUE, 0}
+        };
+
+        /*FOR ANALYSIS:*/ long startTime = System.currentTimeMillis();
+
+        Graph G1 = new Graph(graph);
+        int Cost = G1.mstPrimes(0);
+
+        /*FOR ANALYSIS:*/ long endTime = System.currentTimeMillis();
+
+        System.out.printf("cost %d\n", Cost);
+        G1.printMST();
+        /*FOR ANALYSIS:*/ long executionTime = endTime - startTime;
+
+        /*FOR ANALYSIS:*/ System.out.println("Total Execution Time: " + executionTime + " ms");
+    }
+}
+
+/*
+
+        int a = 100;
+        int graph[][] = new int[a][a];
+
+        for(int i=0;i<a;i++){
+            for(int j=0;j<a;j++){
+                if(i==j){
+                    graph[i][j] = 0;
+                }else{
+                    int var;
+                    graph[i][j] = graph[j][i] = (var=(int) (Math.random() * 10))==0?Integer.MAX_VALUE:var;
+                }
+            }
+        }
+
+
+        /*FOR ANALYSIS:*\/ long startTime = System.currentTimeMillis();
+
+        Graph G1 = new Graph(graph);
+        int Cost = G1.mstPrimes(0);
+
+        /*FOR ANALYSIS:*\/ long endTime = System.currentTimeMillis();
+
+        System.out.printf("cost %d\n", Cost);
+        G1.printMST();
+        /*FOR ANALYSIS:*\/ long executionTime = endTime - startTime;
+
+        /*FOR ANALYSIS:*\/ System.out.println("Total Execution Time: " + executionTime + " ms");
+
+ */
